f98c8eeb02197dff103ac9bdcc60f625085512a9
Shum <3082173487@qq.con>
2025-10-18 15:05:21 +0800

    test gemini llm loading

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-from docx import Document
-import re
-import requests
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-def read_shopping_list(file_path):
-    shopping_items = []
-    try:
-        document = Document(file_path)
+api_key = os.getenv("GEMINI_API_KEY")
+print(api_key)
 
-    except FileNotFoundError as e:
-        print("Error! file not found!!: {e}")
-
-    for paragraph in document.paragraphs:
-        text = paragraph.text
-        pattern = r'\(x(\d+)\)\s*(https?://[^\s]+)'
-        item_matches = re.findall(pattern, text)
-        for count, url in item_matches:
-            shopping_items.append([int(count), url])
-    
-    return shopping_items
-
-
-    
-    
-
-
-read_shopping_list('./origin.docx')
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-pro",
+    api_key = api_key
+)
